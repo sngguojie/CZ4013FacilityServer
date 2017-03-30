5b3d2e764cf1b5e59523cfd349f40b21d7db48f2
Daniel Seetoh <danielseetoh92@gmail.com>
2017-03-31 01:42:44 +0800

    add binder class

---
+++ src/com/cz4013/server/Binder.java
@@ -0,0 +1,18 @@
+package com.cz4013.server;
+
+import java.util.HashMap;
+
+/**
+ * Created by danielseetoh on 3/31/17.
+ */
+public class Binder {
+    private HashMap<String, RemoteObject> objectReferenceHashMap = new HashMap<String, RemoteObject>();
+
+    public void addObjectReference(String name, RemoteObject objRef){
+        this.objectReferenceHashMap.put(name, objRef);
+    }
+
+    public RemoteObject getObjectReference(String name){
+        return this.objectReferenceHashMap.get(name);
+    }
+}
